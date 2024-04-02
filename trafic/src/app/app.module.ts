6f48867a66f9e23723d1cfe41740f710efa19250
suhassuhasgowdabr <164495051+suhassuhasgowdabr@users.noreply.github.com>
2024-04-02 17:37:34 +0800

    Add files via upload

---
+++ trafic/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Header1Component } from './header1/header1.component';
+import { Rou1Component } from './rou1/rou1.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Header1Component,
+    Rou1Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
